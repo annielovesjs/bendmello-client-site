1088ef81de55be96a8636b07bd4e986aa7652761
Annie Liang <aliang27@uwo.ca>
2020-01-17 16:16:11 -0500

    old client site

---
+++ public/benButtons.js
@@ -0,0 +1,39 @@
+$(document).ready(function (){
+    $("#home").click(function (){
+        $('html, body').animate({
+            scrollTop: $(".jumbotron").offset().top
+        }, 2000);
+    });
+});
+
+$(document).ready(function (){
+    $("#work").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#myWork").offset().top
+        }, 2000);
+    });
+});
+
+$(document).ready(function (){
+    $("#about").click(function (){
+        $('html, body').animate({
+            scrollTop: $(".imbenPic").offset().top
+        }, 2000);
+    });
+});
+
+$(document).ready(function (){
+    $("#contact").click(function (){
+        $('html, body').animate({
+            scrollTop: $(".contactForm").offset().top
+        }, 2000);
+    });
+});
+
+$(document).ready(function (){
+    $("#logo").click(function (){
+        $('html, body').animate({
+            scrollTop: $(".jumbotron").offset().top
+        }, 2000);
+    });
+});
